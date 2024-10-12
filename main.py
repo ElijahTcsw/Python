2166abbb24cf7f5a2378b53aad878da04704e6f4
ElijahTcsw <tcs.westervillecoderschool+elijah@gmail.com>
2024-10-12 11:24:41 -0400

    finish

--- main.py
+++ main.py
@@ -6,34 +6,59 @@ pygame.init()
 pygame.font.init()
 numberfont = pygame.font.Font("data-latin.ttf",40)
 windowsurface = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Minesweeper")
 running = True
+death = False
+haswon = False
 grid = Gridgenerator.creategrid(9, 9)
+tile = pygame.image.load("tile.png")
+flag = pygame.image.load("flag.png")
+mine = pygame.image.load("mine.png")
+tileempty = pygame.image.load("tileempty.png")
 while running:
     for tiley in range(0,9):
         for tilex in range(0, 9):
             if grid[tiley][tilex] == 1:
-                pygame.draw.rect(windowsurface,(255, 0, 0),(tilex * 55, tiley * 55, 55, 55))
+                windowsurface.blit(tile,(tilex * 55, tiley * 55))
+                if death:
+                    windowsurface.blit(mine,(tilex * 55, tiley * 55))
             elif grid[tiley][tilex] == 2:
-                pygame.draw.rect(windowsurface,(0, 255, 0),(tilex * 55, tiley * 55, 55, 55))
+                windowsurface.blit(tileempty,(tilex * 55, tiley * 55))
                 if tilecheck.touchingmine(grid, tilex, tiley) > 0:
                     numbersurface = numberfont.render(str(tilecheck.touchingmine(grid, tilex, tiley)),False,(255,255,255))
                     windowsurface.blit(numbersurface, (55 * tilex,55 * tiley))
             elif not grid[tiley][tilex]== 0:
-                pygame.draw.rect(windowsurface,(0, 0, 255),(tilex * 55, tiley * 55, 55, 55))
+                windowsurface.blit(flag,(tilex * 55, tiley * 55))
+                if death and grid[tiley][tilex]== 3:
+                    windowsurface.blit(mine,(tilex * 55, tiley * 55))
             elif grid[tiley][tilex]== 0:
-                pygame.draw.rect(windowsurface,(0, 0, 0),(tilex * 55, tiley * 55, 55, 55))
+                windowsurface.blit(tile,(tilex * 55, tiley * 55))
+    if death:
+        gameover = numberfont.render("You Died",False,(255, 0, 0))
+        windowsurface.blit(gameover, (180, 220))
+    if haswon:
+        gameover = numberfont.render("You Won",False,(0, 255, 0))
+        windowsurface.blit(gameover, (180, 220))
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN and not death and not haswon:
             mousex, mousey = pygame.mouse.get_pos()
             tilex, tiley = Gridgenerator.findtile(mousex, mousey)
             if event.button == 1:
-                if grid[tiley][tilex] == 1:
-                    running = False
+                if grid[tiley][tilex] == 1 or grid[tiley][tilex] == 3:
+                    death = True
                 else:
+                    if grid[tiley][tilex] == 3 or grid[tiley][tilex] == 4:
+                        flags += 1
                     grid[tiley][tilex] = 2
             elif event.button == 3:
-                grid[tiley][tilex] = tilecheck.checkflags(grid, tilex, tiley)
+                flagresult = tilecheck.checkflags(grid, tilex, tiley, flags)
+                if flagresult == 3 or flagresult == 4:
+                    flags -= 1
+                elif grid[tiley][tilex] == 3 or grid[tiley][tilex] == 4:
+                    flags += 1
+                grid[tiley][tilex] = flagresult
+    haswon= tilecheck.victory(grid)
 pygame.quit()
--- tilecheck.py
+++ tilecheck.py
@@ -1,9 +1,15 @@
 
-def checkflags(grid, tilex, tiley):
+def checkflags(grid, tilex, tiley, flags):
     if grid[tiley][tilex] == 1:
-        return 3
+        if flags > 0:
+            return 3
+        else:
+            return 1
     elif grid[tiley][tilex] == 0:
-        return 4
+        if flags > 0:
+            return 4
+        else: 
+            return 0
     elif grid[tiley][tilex] == 3:
         return 1
     elif grid[tiley][tilex] == 4:
@@ -17,6 +23,13 @@ def touchingmine(grid, tilex, tiley):
         for checky in range(-1, 2):
             finaly= tiley + checky
             if (finalx >= 0 and finalx< 9 and finaly >= 0 and finaly< 9):
-                if grid[finaly][finalx] == 1:
+                if grid[finaly][finalx] == 1 or grid[finaly][finalx] == 3:
                     foundtiles += 1
     return foundtiles
+def victory(grid):
+    for tiley in range (0, 9):
+        for tilex in range (0, 9):
+            if grid[tiley][tilex] == 1 or grid[tiley][tilex]== 0:
+                return False
+    return True
+            
